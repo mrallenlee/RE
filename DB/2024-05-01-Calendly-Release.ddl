a896c295119dded02d6f8629cd5bb37356a8cf7c
lmark-allen <mr.allen.lee@gmail.com>
2024-04-30 10:16:40 -0400

    Add calendly integration wrapper. Add generate random name SQL function Add calendly integration DDL

---
+++ DB/2024-05-01-Calendly-Release.ddl
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS `Appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Appointment` (
+  `CalendlyID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `UserID` bigint(20) NOT NULL DEFAULT '0',
+  `Email` varchar(255),
+  `ApptPlatform` varchar(25) NOT NULL DEFAULT 'Calendly',
+  `ApptType` varchar(25) NOT NULL DEFAULT 'PDI',
+  `ApptDate` date NOT NULL DEFAULT '0000-00-00',
+  `ApptStartTime` timestamp NOT NULL DEFAULT '0',
+  `ApptEndTime` timestamp NOT NULL DEFAULT '0',
+  `ApptStatus` varchar(25),
+  `MeetingURL` varchar(1024),
+  `RescheduleURL` varchar(1024),
+  `UpdatedBy` varchar(25) NOT NULL DEFAULT '',
+  `LastModifiedDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `CreatedDate` date NOT NULL DEFAULT '0000-00-00',
+  `OrgJSON` TEXT,  
+  PRIMARY KEY (`CalendlyID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `CalendlyLanding`;
+CREATE TABLE `CalendlyLanding` (
+  `OrgJSON` TEXT,  
+  `CID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `UpdatedBy` varchar(25) NOT NULL DEFAULT '',
+  `LastModifiedDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `CreatedDate` date NOT NULL DEFAULT '0000-00-00',
+  PRIMARY KEY (`CID`)  
+)  ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
---
+++ DB/GenerateName.sql
@@ -0,0 +1,18 @@
+DROP function if exists generate_fname;
+DELIMITER $$
+CREATE FUNCTION generate_fname () RETURNS varchar(255)
+BEGIN
+	RETURN ELT(FLOOR(1 + (RAND() * (100-1))), "James","Mary","John","Patricia","Robert","Linda","Michael","Barbara","William","Elizabeth","David","Jennifer","Richard","Maria","Charles","Susan","Joseph","Margaret","Thomas","Dorothy","Christopher","Lisa","Daniel","Nancy","Paul","Karen","Mark","Betty","Donald","Helen","George","Sandra","Kenneth","Donna","Steven","Carol","Edward","Ruth","Brian","Sharon","Ronald","Michelle","Anthony","Laura","Kevin","Sarah","Jason","Kimberly","Matthew","Deborah","Gary","Jessica","Timothy","Shirley","Jose","Cynthia","Larry","Angela","Jeffrey","Melissa","Frank","Brenda","Scott","Amy","Eric","Anna","Stephen","Rebecca","Andrew","Virginia","Raymond","Kathleen","Gregory","Pamela","Joshua","Martha","Jerry","Debra","Dennis","Amanda","Walter","Stephanie","Patrick","Carolyn","Peter","Christine","Harold","Marie","Douglas","Janet","Henry","Catherine","Carl","Frances","Arthur","Ann","Ryan","Joyce","Roger","Diane");
+END$$
+
+DELIMITER ;
+
+DROP function if exists generate_lname;
+DELIMITER $$
+CREATE FUNCTION generate_lname () RETURNS varchar(255)
+BEGIN
+	RETURN ELT(FLOOR(1 + (RAND() * (100-1))), "Smith","Johnson","Williams","Jones","Brown","Davis","Miller","Wilson","Moore","Taylor","Anderson","Thomas","Jackson","White","Harris","Martin","Thompson","Garcia","Martinez","Robinson","Clark","Rodriguez","Lewis","Lee","Walker","Hall","Allen","Young","Hernandez","King","Wright","Lopez","Hill","Scott","Green","Adams","Baker","Gonzalez","Nelson","Carter","Mitchell","Perez","Roberts","Turner","Phillips","Campbell","Parker","Evans","Edwards","Collins","Stewart","Sanchez","Morris","Rogers","Reed","Cook","Morgan","Bell","Murphy","Bailey","Rivera","Cooper","Richardson","Cox","Howard","Ward","Torres","Peterson","Gray","Ramirez","James","Watson","Brooks","Kelly","Sanders","Price","Bennett","Wood","Barnes","Ross","Henderson","Coleman","Jenkins","Perry","Powell","Long","Patterson","Hughes","Flores","Washington","Butler","Simmons","Foster","Gonzales","Bryant","Alexander","Russell","Griffin","Diaz","Hayes");
+END$$
+DELIMITER ;
+
+select generate_fname() as FirstName, generate_fname() as LastName;
