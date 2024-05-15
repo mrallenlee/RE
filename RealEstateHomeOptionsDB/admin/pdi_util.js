334c1411260b605b2e2255341e64b0e82c75f7b5
lmark-allen <mr.allen.lee@gmail.com>
2024-05-15 11:52:37 -0400

    Partial finish on day 30 report with signature pad adding pdi_util.js

---
+++ RealEstateHomeOptionsDB/admin/pdi_util.js
@@ -0,0 +1,26 @@
+
+
+	// PDI Book appoint email feature
+	function emailBookPDIAppt(unitNumber, userID){
+		// alert("emailBookPDI unitNumber=" + unitNumber);
+        $.ajax({
+            url: 'pdi_util.phtml',
+            data: 'action=emailBookPDIAppt&unitNumber=' + unitNumber + '&userID=' + userID,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                alert("Email sent successfully");
+                }
+            ,
+            error: function (data) {
+				alert('Error: This is an error in processing the data. Please contact system administrator.');
+
+				//alert('sql=' + data['status']);
+				//alert('fuck=' + data['fuck']);
+				alert('error=' + data);
+				// display_user_message("[ <font color=#009900><b>Error at updating records.</b></font> ]<br>\n<br>");
+            }});
+        }
+        
+
+	
